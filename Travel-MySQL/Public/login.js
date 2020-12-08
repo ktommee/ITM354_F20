a5a7584a21d557035e85eb8ad08bd7d141515e07
Kristina Tommee <ktommee@hawaii.edu>
2020-12-07 23:57:43 -1000

    Update login.js

--- Travel-MySQL/Public/login.js
+++ Travel-MySQL/Public/login.js
@@ -51,14 +51,14 @@ footer.innerHTML = (`
     <table>
     <tbody>
     <tr>
-        <td><img src="https://image.flaticon.com/icons/png/512/2874/2874771.png"width="75"> nobu@nobusakurai.com</td>
+        <td><img src="https://image.flaticon.com/icons/png/512/2874/2874771.png"width="30"> nobu@nobusakurai.com</td>
         
     </tr>
     <tr>
-    <td><img src="https://image.flaticon.com/icons/png/512/2939/2939444.png"width="75"> (808)636-0143</td>
+    <td><img src="https://image.flaticon.com/icons/png/512/2939/2939444.png"width="30"> (808)636-0143</td>
   </tr>
   <tr>
-  <td><img src="https://image.flaticon.com/icons/png/512/2143/2143101.png"width="85">98-1238 Kaahumanu Ave., Ste. 201 Pearl City, HI</td>
+  <td><img src="https://image.flaticon.com/icons/png/512/2143/2143101.png"width="30">98-1238 Kaahumanu Ave., Ste. 201 Pearl City, HI</td>
     </tr>
     </tbody>	
 </div>
